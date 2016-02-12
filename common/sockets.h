3b40fe32b76366a55268ae17ddbc8c05e2d3cb03
kuhar <kubakuderski@gmail.com>
2016-02-12 18:37:42 +0100

    add cmake and projects' structure

---
+++ common/sockets.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
---
+++ server/server.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "common/sockets.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello server\n";
+}
