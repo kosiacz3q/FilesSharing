9f84953070d99a6f109941d8fbe280b78fc28eae
kuhar <kubakuderski@gmail.com>
2016-02-13 12:50:19 +0100

    further fixes to threading

--- common/client_socket_manager.cpp
+++ common/client_socket_manager.cpp
@@ -6,7 +6,7 @@
 #include "utils.h"
 
 ClientSocketManager::ClientSocketManager(ClientSocket&& socket)
-    : mContext(std::make_shared<Context>(std::move(socket))){
+    : mContext(std::make_shared<Context>(std::move(socket))) {
     assert(mContext->mSocket.isValid());
     loop();
 }
@@ -17,25 +17,25 @@ ClientSocketManager::~ClientSocketManager() {
 
 bool ClientSocketManager::isOutgoingBufferEmpty() {
     mutex_guard _(mContext->mOutgoingMutex);
-    return mOutgoingBuffer.empty();
+    return mContext->mOutgoingBuffer.empty();
 }
 
 bool ClientSocketManager::isIncomingBufferEmpty() {
     mutex_guard _(mContext->mIncomingMutex);
-    return mIncomingBuffer.empty();
+    return mContext->mIncomingBuffer.empty();
 }
 
 std::vector<char> ClientSocketManager::pop() {
     mutex_guard _(mContext->mIncomingMutex);
     assert(!mIncomingBuffer.empty());
-    auto res = mIncomingBuffer.front();
-    mIncomingBuffer.erase(mIncomingBuffer.begin());
+    auto res = mContext->mIncomingBuffer.front();
+    mContext->mIncomingBuffer.erase(mContext->mIncomingBuffer.begin());
     return res;
 }
 
 void ClientSocketManager::push(std::vector<char> payload) {
     mutex_guard _(mContext->mOutgoingMutex);
-    mOutgoingBuffer.push_back(std::move(payload));
+    mContext->mOutgoingBuffer.push_back(std::move(payload));
 }
 
 void ClientSocketManager::stop() {
@@ -45,16 +45,16 @@ void ClientSocketManager::stop() {
 
 void ClientSocketManager::loop() {
     using namespace std::chrono_literals;
-    std::thread sender([&, ctx = mContext]{
-       while (!mContext->mStop) {
+    std::thread sender([ctx = mContext]{
+       while (!ctx->mStop) {
            {
                std::lock(ctx->mOutgoingMutex, ctx->mSocketMutex);
                mutex_guard _1(ctx->mOutgoingMutex, std::adopt_lock);
                mutex_guard _2(ctx->mSocketMutex, std::adopt_lock);
-               if (!mOutgoingBuffer.empty()) {
-                   bool res = ctx->mSocket.send(mOutgoingBuffer.front());
+               if (!ctx->mOutgoingBuffer.empty()) {
+                   bool res = ctx->mSocket.send(ctx->mOutgoingBuffer.front());
                    (void) res;
-                   mOutgoingBuffer.erase(mOutgoingBuffer.begin());
+                   ctx->mOutgoingBuffer.erase(ctx->mOutgoingBuffer.begin());
                }
            }
            std::this_thread::sleep_for(5ms);
@@ -62,14 +62,14 @@ void ClientSocketManager::loop() {
     });
     sender.detach();
 
-    std::thread receiver([&, ctx = mContext]{
+    std::thread receiver([ctx = mContext]{
         while (!ctx->mStop) {
             {
                 std::lock(ctx->mIncomingMutex, ctx->mSocketMutex);
                 mutex_guard _1(ctx->mIncomingMutex, std::adopt_lock);
                 mutex_guard _2(ctx->mSocketMutex, std::adopt_lock);
                 auto res = ctx->mSocket.receive();
-                if (res) mIncomingBuffer.push_back(*res);
+                if (res) ctx->mIncomingBuffer.push_back(*res);
             }
             std::this_thread::sleep_for(5ms);
         }
--- common/client_socket_manager.h
+++ common/client_socket_manager.h
@@ -19,20 +19,19 @@ public:
     void stop();
 
 private:
-
     void loop();
 
-    std::vector<std::vector<char>> mIncomingBuffer;
-    std::vector<std::vector<char>> mOutgoingBuffer;
-
     struct Context {
-
         Context(ClientSocket&& socket) : mSocket(std::move(socket)) {}
+
+        std::vector<std::vector<char>> mIncomingBuffer;
+        std::vector<std::vector<char>> mOutgoingBuffer;
 
         std::mutex mIncomingMutex;
         std::mutex mOutgoingMutex;
         std::mutex mSocketMutex;
         std::atomic<bool> mStop{false};
+
         ClientSocket mSocket;
     };
     std::shared_ptr<Context> mContext;
